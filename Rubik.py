51d6e24685ae03a8fd7720594c44767aee42e5b0
charlesmasserann <charloxd@gmail.com>
2024-06-14 17:22:16 +0200

    Fill Rubik class

--- Rubik.py
+++ Rubik.py
@@ -2,14 +2,94 @@ class Rubik:
     '''Rubik class'''
 
     def __init__(self):
-        '''Rubik Constructor'''
+        # Utilisation d'entiers pour stocker les positions et orientations des coins et arêtes
+        self.corners = 0
+        self.edges = 0
+        self.init_rubik()
 
-    def get_args(self):
-        '''Rubik args getter'''
+    def init_rubik(self):
+        # Initialiser les coins et arêtes pour un cube résolu
+        # Coins: 3 bits pour la position et 3 bits pour l'orientation
+        for i in range(8):
+            self.corners |= (i << (i * 3))  # Position i avec orientation 0
 
-        return self.args
+        # Arêtes: 4 bits pour la position et 1 bit pour l'orientation
+        for i in range(12):
+            self.edges |= (i << (i * 2))  # Position i avec orientation 0
 
-    def set_args(self, args):
-        '''Rubik args setter'''
-    
-        self.args = args     
+    def print_rubik(self):
+        # Afficher les positions et orientations des coins et arêtes
+        for i in range(8):
+            print(f"Corner {i}: {self.get_corner(i)}")
+        for i in range(12):
+            print(f"Edge {i}: {self.get_edge(i)}")
+
+    def get_corner(self, index):
+        # Obtenir la position et l'orientation d'un coin
+        shift = index * 3
+        mask = 0b111 << shift
+        return (self.corners & mask) >> shift
+
+    def set_corner(self, index, value):
+        # Définir la position et l'orientation d'un coin
+        shift = index * 3
+        mask = ~(0b111 << shift)
+        self.corners = (self.corners & mask) | (value << shift)
+
+    def get_edge(self, index):
+        # Obtenir la position et l'orientation d'une arête
+        shift = index * 2
+        mask = 0b11 << shift
+        return (self.edges & mask) >> shift
+
+    def set_edge(self, index, value):
+        # Définir la position et l'orientation d'une arête
+        shift = index * 2
+        mask = ~(0b11 << shift)
+        self.edges = (self.edges & mask) | (value << shift)
+
+    def move_U(self):
+        # Exemple de mouvement U (face supérieure)
+        # Permuter les coins et arêtes affectés
+        affected_corners = [0, 1, 2, 3]
+        affected_edges = [0, 1, 2, 3]
+
+        # Rotation des coins
+        temp = self.get_corner(affected_corners[0])
+        for i in range(3):
+            self.set_corner(affected_corners[i], self.get_corner(affected_corners[i + 1]))
+        self.set_corner(affected_corners[3], temp)
+
+        # Rotation des arêtes
+        temp = self.get_edge(affected_edges[0])
+        for i in range(3):
+            self.set_edge(affected_edges[i], self.get_edge(affected_edges[i + 1]))
+        self.set_edge(affected_edges[3], temp)
+
+    def solve_phase1(self):
+        # Résoudre la phase 1
+        pass
+
+    def solve_phase2(self):
+        # Résoudre la phase 2
+        pass
+
+    def solve_phase3(self):
+        # Résoudre la phase 3
+        pass
+
+    def solve_phase4(self):
+        # Résoudre la phase 4
+        pass
+
+    def solve(self):
+        self.solve_phase1()
+        self.solve_phase2()
+        self.solve_phase3()
+        self.solve_phase4()
+
+
+cube = Rubik()
+cube.print_rubik()
+cube.move_U()
+cube.print_rubik()
